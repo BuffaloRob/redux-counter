d9aabaa5875d9c39862876b3b4dba338dca5d006
BuffaloRob <peckham.rob@gmail.com>
2018-11-20 11:24:02 -0500

    added payload to action

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 
 const reducer = (state, action) => {
   if (action.type === 'INCREMENT') {
-    return state + 1;
+    return state + action.amount;
   } else if (action.type === 'DECREMENT') {
-    return state -1;
+    return state - action.amount;
   } else {
     return state;
   }
 }
 
-const incrementAction = { type: 'INCREMENT' };
+const incrementAction = { 
+  type: 'INCREMENT',
+  amount: 7
+};
 
 console.log(reducer(0, incrementAction));
 console.log(reducer(1, incrementAction));
