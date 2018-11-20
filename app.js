b9c3c87b61b8e0b7aef060f027b395877c5eeb52
BuffaloRob <peckham.rob@gmail.com>
2018-11-20 14:56:43 -0500

    + actions to see change in store state

--- app.js
+++ app.js
@@ -23,3 +23,23 @@ const createStore = (reducer) => {
     dispatch,
   };
 }
+
+const store = createStore(reducer);
+
+const incrementAction = {
+  type: 'INCREMENT',
+  amount: 3
+}
+
+store.dispatch(incrementAction);
+console.log(store.getState());
+store.dispatch(incrementAction);
+console.log(store.getState());
+
+const decrementAction = {
+  type: "DECREMENT",
+  amount: 4
+};
+
+store.dispatch(decrementAction);
+console.log(store.getState());
